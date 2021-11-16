1574e5bb66cd6ee0b9669f916fabaa8121da7ced
Kirill Fedyakov <vogelfenix@gmail.com>
2021-11-16 23:53:28 +0100

    rename variable

--- src/fetch_media/download_file.py
+++ src/fetch_media/download_file.py
@@ -6,7 +6,7 @@ from common.utilities import get_filename_from_url
 
 
 def download_image(image_url,
-                   image_path_to_save=settings.images_dir_path,
+                   images_dir_path=settings.images_dir_path,
                    image_name='',
                    parameters={}):
 
@@ -16,6 +16,6 @@ def download_image(image_url,
     if not image_name:
         image_name = f'{get_filename_from_url(image_url)}'
 
-    image_path = Path(image_path_to_save) / image_name
+    image_path = Path(images_dir_path) / image_name
     with open(image_path, 'wb') as file:
         file.write(response.content)
